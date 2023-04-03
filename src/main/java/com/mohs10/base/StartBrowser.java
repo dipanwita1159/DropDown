619a694c71816467c7710daeb2286434221e7a64
dipanwita1159 <112863643+dipanwita1159@users.noreply.github.com>
2023-04-03 15:21:17 +0530

    Update StartBrowser.java

--- src/main/java/com/mohs10/base/StartBrowser.java
+++ src/main/java/com/mohs10/base/StartBrowser.java
@@ -40,12 +40,12 @@ public class StartBrowser {
 	 }
 	  @BeforeClass
 	  public void beforeClass() {
-		  // WebDriverManager.chromedriver().setup();
+		   WebDriverManager.chromedriver().setup();
 		 // WebDriverManager.firefoxdriver().setup();
-		  WebDriverManager.edgedriver().setup();
-		 // driver = new ChromeDriver();
+		  //WebDriverManager.edgedriver().setup();
+		  driver = new ChromeDriver();
 		// driver= new FirefoxDriver();
-		  driver=new EdgeDriver();
+		 // driver=new msedgeDriver();
 		  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		  driver.manage().window().maximize();
 	  }
