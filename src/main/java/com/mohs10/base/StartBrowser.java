3d2534d48a0282a40bf181cb3964e91d8947e08d
dipanwita1159 <112863643+dipanwita1159@users.noreply.github.com>
2023-04-03 17:31:10 +0530

    Update StartBrowser.java

--- src/main/java/com/mohs10/base/StartBrowser.java
+++ src/main/java/com/mohs10/base/StartBrowser.java
@@ -41,9 +41,15 @@ public class StartBrowser {
 	  @BeforeClass
 	  public void beforeClass() {
 		  
+		  
 		   WebDriverManager.chromedriver().setup();
 		  ChromeOptions options = new ChromeOptions();
-    options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+options.addArguments("binary", "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
+
+WebDriver driver = new ChromeDriver(options);
+
+		  //ChromeOptions options = new ChromeOptions();
+   // options.setBinary("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe");
 		 // WebDriverManager.firefoxdriver().setup();
 		  //WebDriverManager.edgedriver().setup();
 		  driver = new ChromeDriver(options);
